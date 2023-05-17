4ca31ea21aa72cc77e67c708fdac40e637fa4704
CHRISTIAN S BARBOSA <32348713+djchristiandf@users.noreply.github.com>
2023-05-17 15:35:20 -0300

    finalizado o save usuario

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# Symfony 6 Twig 3 PHP 8
+# Symfony 6 Twig 3 PHP 8 - Bootstrap 4
 
 ## Projeto DIO
 
-Projeto desenvolvido no curso php avançado da DIO
+Projeto desenvolvido no curso php avançado da DIO , crud de usuario usando sqlite 3
+
+- composer require twig
+- composer require symfony/orm-pack
+- composer require --dev symfony/maker-bundle
+- composer require --dev symfony/var-dumper
+- Lembrando na versao symfony 6 usa o EntityManagerInterface $entityManager para persistir dados no banco
+- verifica se tem dados no banco: php bin/console dbal:run-sql 'SELECT * FROM usuario'
--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Usuario;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +15,38 @@ class UsuarioController extends AbstractController
     #[Route('/', methods: ['GET'], name: 'index')]
     public function index() : Response
     {
-        return $this->render('usuario/form.html.twig');
+        return $this->render('usuario/form.html.twig', [
+            'fulano' => "Christian"
+        ]);
     }
 
     #[Route('/salvar', methods: ['POST'], name : 'salvar')]
-    public function salvar(): Response
+    public function salvar(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return new Response("imlementar gravação no banco");
+        // dump($request->request->ALL());
+        $data = $request->request->ALL();
+
+        $usuario = new Usuario();
+        $usuario->setNome($data['nome']);
+        $usuario->setEmail($data['email']);
+
+        dump($usuario);
+        $entityManager->persist($usuario);
+        $entityManager->flush();
+        dump($usuario);
+
+        
+        if($usuario->getId()) //outra opcao de verificar se persistiu no db $entityManager->contains($usuario)
+        {
+            return $this->render("usuario/sucessform.html.twig", [
+                'fulano' => $data['nome']
+            ]);
+        }else{
+            return $this->render("usuario/errorform.html.twig", [
+                'fulano' => $data['nome']
+            ]);
+        }
+
+        
     }
 }
